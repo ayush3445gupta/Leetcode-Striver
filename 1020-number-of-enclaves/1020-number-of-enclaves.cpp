9ba09ff6bb5981a941b617de0a21d3291e68bf2b
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-01-15 16:47:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,60 +1,54 @@
 class Solution {
-private:
-    void dfs(int row, int col, vector<vector<int>>& vis,
-             vector<vector<int>>& grid, int delR[], int delC[]) {
-        vis[row][col] = 1;
- int n = grid.size();
-        int m = grid[0].size();
-
-        for (int i = 0; i < 4; i++) {
-            int rrow = row + delR[i];
-
-            int ccol = col + delC[i];
-
-            if (rrow >= 0 && rrow < n && ccol >= 0 && ccol < m &&
-                vis[rrow][ccol] == 0 && grid[rrow][ccol] == 1) {
-                dfs(rrow, ccol, vis, grid, delR, delC);
-            }
-        }
-    }
-
 public:
-    int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+    void dfs(int row,int col,vector<vector<int>>& grid,vector<vector<int>>&vis,int delrow[],int delcol[]){
+    vis[row][col]=1;
+    int n=grid.size(),m=grid[0].size();
+    for (int i = 0; i <4; i++)
+    {
+        int nrow=row+delrow[i],ncol=col+delcol[i];
+        if(nrow>=0 && row<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+            dfs(nrow,ncol,grid,vis,delrow,delcol);
+        } 
+    }   
+}
+int numEnclaves(vector<vector<int>>& grid) {
+    /*
+    LOGIC = Traverse all boundary and mark all 1s including their neighbours present as visited using dfs, then traverse whole grid and count all unvisited 1s and return.
+    */
+    int n=grid.size(),m=grid[0].size();
+    vector<vector<int>>vis(n,vector<int>(m,0));
+    int delrow[]={-1,0,1,0},delcol[]={0,1,0,-1};
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        int delR[] = {-1, 0, 1, 0};
-        int delC[] = {0, 1, 0, -1};
-
-        for (int i = 0; i < m; i++) {
-            if (grid[0][i] == 1 && vis[0][i] == 0) {
-                dfs(0, i, vis, grid, delR, delC);
-            }
-
-            if (grid[n - 1][i] == 1 && vis[n - 1][i] == 0) {
-                dfs(n - 1, i, vis, grid, delR, delC);
-            }
+    // traverse first and last row
+    for (int j = 0; j < m; j++)
+    {
+        if(vis[0][j]==0 && grid[0][j]==1){
+            dfs(0,j,grid,vis,delrow,delcol);
         }
-
-        for (int i = 0; i < n; i++) {
-            if (grid[i][0] == 1 && vis[i][0] == 0) {
-                dfs(i, 0, vis, grid, delR, delC);
-            }
-
-            if (grid[i][m - 1] == 1 && vis[i][m - 1] == 0) {
-                dfs(i, m - 1, vis, grid, delR, delC);
-            }
+        if(vis[n-1][j]==0 && grid[n-1][j]==1){
+            dfs(n-1,j,grid,vis,delrow,delcol);
         }
-
-        int cnt = 0;
-        for (int i = 0;i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && vis[i][j] == 0) {
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
     }
+    // traverse first and last col
+    for (int i = 0; i < n; i++)
+    {
+        if(vis[i][0]==0 && grid[i][0]==1){
+            dfs(i,0,grid,vis,delrow,delcol);
+        }
+
+        if(vis[i][m-1]==0 && grid[i][m-1]==1){
+            dfs(i,m-1,grid,vis,delrow,delcol);
+        }
+    }
+    int cnt=0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j< m; j++)
+        {
+            if(vis[i][j]==0 && grid[i][j]==1)cnt++;
+        }
+        
+    }
+    return cnt;
+}
 };
