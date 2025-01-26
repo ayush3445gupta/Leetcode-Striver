4eb584d6783eb45f4658b1edfe1e58b84c2312b8
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-01-26 14:26:17 +0530

    Time: 1 ms (24.49%), Space: 9.4 MB (33.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,21 +14,41 @@ public:
 //     vector<vector<int>>dp(m,vector<int>(n,-1));
 //     return f(m-1,n-1,dp);
 // }.
+// int uniquePaths(int m, int n) {
+//     vector<vector<int>>dp(m,vector<int>(n,-1));
+//     for (int row= 0; row < m; row++)dp[row][0]=1;
+//     for (int col= 0; col < n; col++)dp[0][col]=1;
+
+//     for (int i = 1; i <m; i++)
+//     {
+//         for (int j = 1; j < n; j++)
+//         {
+//            int down=dp[i-1][j];
+//            int right=dp[i][j-1];
+
+//            dp[i][j]=down+right;
+//         }    
+//     }
+//     return dp[m-1][n-1];    
+// }
 int uniquePaths(int m, int n) {
     vector<vector<int>>dp(m,vector<int>(n,-1));
-    for (int row= 0; row < m; row++)dp[row][0]=1;
-    for (int col= 0; col < n; col++)dp[0][col]=1;
+    vector<int>prev(n,1);
+    // for (int row= 0; row < m; row++)dp[row][0]=1;
+    // for (int col= 0; col < n; col++)dp[0][col]=1;
 
     for (int i = 1; i <m; i++)
     {
+        vector<int>temp(n,1);
         for (int j = 1; j < n; j++)
         {
-           int down=dp[i-1][j];
-           int right=dp[i][j-1];
+           int down=prev[j];
+           int right=temp[j-1];
 
-           dp[i][j]=down+right;
-        }    
+           temp[j]=down+right;
+        }  
+        prev=temp;  
     }
-    return dp[m-1][n-1];    
+    return prev[n-1];    
 }
 };
