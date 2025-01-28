0b36e245b7a2886406d85b2fc904081ba360d3cb
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-01-29 00:05:18 +0530

    Time: 0 ms (100%), Space: 15.1 MB (38.77%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     // memoization
-int f(int row,int col,int n,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-    // basecase
-    if(row==0 && col<n && col>=0)return matrix[row][col];
-    if(row<0 || col<0 || col>=n)return 1e9;
-
-    if(dp[row][col]!=-1)return dp[row][col];
-    int up=matrix[row][col]+f(row-1,col,n,matrix,dp);
-    int left=matrix[row][col]+f(row-1,col-1,n,matrix,dp);;
-    int right=matrix[row][col]+f(row-1,col+1,n,matrix,dp);
-
-    return dp[row][col]=min(min(up,left),right);
-}
 int minFallingPathSum(vector<vector<int>>& matrix) {
     int n=matrix.size();
-    vector<vector<int>>dp(n,vector<int>(n,-1));
-    int ans=INT_MAX;
-    for (int j = 0; j < n; j++)
+    vector<vector<int>>dp(n,vector<int>(n,0));
+    for (int i = 0; i <n; i++) dp[0][i]=matrix[0][i];
+
+    for (int i = 1; i <n; i++)
     {
-        ans=min(ans,f(n-1,j,n,matrix,dp));
+        for (int j = 0; j < n; j++)
+        {
+            int down=matrix[i][j]+dp[i-1][j];
+            int left=matrix[i][j];
+            if(j>=1)left+=dp[i-1][j-1];
+            else left+=1e9;
+            int right = matrix[i][j];
+            if(j<n-1)right+=dp[i-1][j+1];
+            else right+=1e9;
+
+            dp[i][j]=min(min(down,left),right);
+        }
+    }
+    int ans=INT_MAX;
+    for (int j = 0; j< n; j++)
+    {
+        ans=min(ans,dp[n-1][j]);
     }
     return ans;
 }
