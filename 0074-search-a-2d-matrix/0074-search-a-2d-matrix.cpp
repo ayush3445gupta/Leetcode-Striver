44a29cdbfa74be44df43564182a7763ae4148cf1
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-05-21 23:27:31 +0530

    Time: 0 ms (100%), Space: 13.2 MB (97.75%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int m=matrix.size(),n=matrix[0].size();
+    int n=matrix.size(),m=matrix[0].size();
     // find the row in which target is present
-    int l=0,r=m-1;
-    while (l<=r)
+    for (int i = 0; i < n; i++)
     {
-        int m=(l+r)>>1;
-        if(matrix[m][0]==target)return true;
-        else if (matrix[m][0]>target)r=m-1;
-        else l=m+1;
-    }
-    int row=r;
-    int l2=0,r2=n-1;
-    while (l2<=r2)
-    {
-        int m2=(l2+r2)>>1;
-        if(matrix[row][m2]==target)return true;
-        else if (matrix[r][m2]>target)r2=m2-1;
-        else l2=m2+1;   
+        if(target>=matrix[i][0] && target<=matrix[i][m-1]){
+            int l=0,r=m-1;
+            while (l<=r)
+            {
+                int m=(l+r)>>1;
+                if(matrix[i][m]==target)return true;
+                else if (matrix[i][m]>target)r=m-1;
+                else l=m+1;   
+            }
+            return false;
+        }
     }
     return false;
     }
