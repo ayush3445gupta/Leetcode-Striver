e1217174b2445d844dea0b8c2d15544324c72684
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-06-18 15:04:12 +0530

    Time: 179 ms (60.88%), Space: 174.5 MB (43.65%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
+void dfs(int node,vector<vector<int>>&adj,vector<int>&vis,int& count){
+    vis[node]=1;
+    count++;
+    for(auto it:adj[node]){
+        if(!vis[it])
+        dfs(it,adj,vis,count);
+    }
+    return;
+}
 int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
   // we will apply BFS
-  unordered_set<int>st(restricted.begin(),restricted.end());
   vector<vector<int>>adj(n);
 
   for(auto it:edges){
@@ -12,21 +20,26 @@ int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
 
     vector<int>vis(n,0);
     vis[0]=1;
+    for(auto it:restricted)vis[it]=1;
     int count=0;
-    queue<int>que;
-    que.push(0);
-    while (!que.empty())
-    {
-        int node=que.front();
-        que.pop();
-        count++;
-        for(auto it:adj[node]){
-            if(!vis[it] && st.find(it)==st.end()){
-                vis[it]=1;
-                que.push(it);
-            }
-        }
-    }
+
+                            // queue<int>que;
+                            // que.push(0);
+                            // while (!que.empty())
+                            // {
+                            //     int node=que.front();
+                            //     que.pop();
+                            //     count++;
+                            //     for(auto it:adj[node]){
+                            //         if(!vis[it] && st.find(it)==st.end()){
+                            //             vis[it]=1;
+                            //             que.push(it);
+                            //         }
+                            //     }
+                            // }
+
+    // DFS
+     dfs(0,adj,vis,count);
 return count;
 }
 };
