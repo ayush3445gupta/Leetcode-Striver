ff398236fe136edcaed22f86bec079eab37d0aa8
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-07-15 09:35:22 +0530

    Time: 3 ms (13.67%), Space: 32.2 MB (39.07%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-int minSubArrayLen(int target, vector<int>& nums) {
-      int n=nums.size();
-      int left=0,right=0;
-      int ans=INT_MAX;
-      while (right<n)
-      {
-        target-=nums[right];
-            while (target<0)
+// int minSubArrayLen(int target, vector<int>& nums) {
+//       int n=nums.size();
+//       int left=0,right=0;
+//       int ans=INT_MAX;
+//       while (right<n)
+//       {
+//         target-=nums[right];
+//             while (target<0)
+//             {
+//                 ans=min(ans,right-left+1);
+//                 target+=nums[left];
+//                 left++;
+//             }
+//        if(target==0){
+//         ans=min(ans,right-left+1);
+//        }
+//         right++;
+//       }
+//       if(ans==INT_MAX)return 0;
+//       return ans;  
+//     }
+
+// 15-07-25
+int minSubArrayLen(int target, vector<int>& nums){
+    // if sum less than target expand to right ,else shrink from
+    // left
+
+    int n=nums.size();
+    int left=0,right=0;
+    int minWindow=INT_MAX;
+    int sum=0;
+    while (right<n)
+    {
+        sum+=nums[right];
+            while (sum>target)
             {
-                ans=min(ans,right-left+1);
-                target+=nums[left];
+                minWindow=min(right-left+1,minWindow);
+                sum-=nums[left];
                 left++;
             }
-       if(target==0){
-        ans=min(ans,right-left+1);
-       }
+            if(sum==target)minWindow=min(right-left+1,minWindow);
         right++;
-      }
-      if(ans==INT_MAX)return 0;
-      return ans;  
     }
+    
+    return (minWindow==INT_MAX?0:minWindow);
+}
 };
