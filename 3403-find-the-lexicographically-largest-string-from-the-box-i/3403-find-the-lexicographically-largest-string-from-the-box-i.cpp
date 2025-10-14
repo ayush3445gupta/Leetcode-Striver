6400dab67bb7876fe86c6444777c0406bc8e4f34
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-10-15 00:10:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
 string answerString(string word, int numFriends) {
       int n=word.length();
-      if(numFriends==1)return word;
+      if(n==1)return word;
       char mx_char='a';
       for (int i = 0; i < n; i++)
         if(word[i]>mx_char)mx_char=word[i];
 
     set<string>vec; 
+    string res;
     for (int i = 0; i < n; i++)
     {
         if(word[i]==mx_char){
             int leftslice=i;
             if(leftslice+1>=numFriends)
-                vec.insert(word.substr(i));
+            res=max(res,word.substr(i));
             else{
             int req=numFriends-leftslice;
             int right=n-req;
             int len=right-i+1;
-            vec.insert(word.substr(i,len));
+            res=max(res,word.substr(i,len));
             }
         }
     }
-    auto it=vec.end();
-    it--;
-    return *it;  
+    return res;  
     }
 };
