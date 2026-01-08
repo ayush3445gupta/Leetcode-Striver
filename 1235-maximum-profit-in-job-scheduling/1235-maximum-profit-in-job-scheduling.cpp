974fdcbb0e61bd0fe33ad5ee9886702e7cb2d116
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2026-01-08 22:34:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,31 +1,61 @@
 class Solution {
 public:
-int n;
-int find(int start,int idx,vector<int>&a){
-    int left=start,right=n-1,ans=n;
-    while (left<=right)
-    {
-        int mid=left+(right-left)/2;
+// int n;
+// int find(int start,int idx,vector<int>&a){
+//     int left=start,right=n-1,ans=n;
+//     while (left<=right)
+//     {
+//         int mid=left+(right-left)/2;
 
-        if(a[mid]<idx)left=mid+1;
-        else{
-            ans=mid;
-            right=mid-1;
+//         if(a[mid]<idx)left=mid+1;
+//         else{
+//             ans=mid;
+//             right=mid-1;
+//         }
+//     }
+//     return ans;
+// }
+// int solve(int i,vector<int>& startTime, vector<int>& endTime, vector<int>& profit,vector<int>&dp){
+//     // basecase
+//     if(i==n)return 0;
+
+//      if( dp[i]!=-1)return dp[i];
+
+//     int skip=solve(i+1,startTime,endTime,profit,dp);
+//     int next_id = find(i,endTime[i],startTime);
+//     int take=profit[i] + solve(next_id,startTime,endTime,profit,dp);
+//     return dp[i]=max(take,skip);
+// }
+// int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+//     n=startTime.size();
+//     vector<int>idx(n);
+//     iota(idx.begin(),idx.end(),0);
+//     sort(idx.begin(),idx.end(),[&](int a,int b){
+//         return startTime[a]<startTime[b];
+//     });
+//     vector<int>st(n),et(n),pf(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         st[i]=startTime[idx[i]];
+//         et[i]=endTime[idx[i]];
+//         pf[i]=profit[idx[i]];
+//     }
+//     vector<int>dp(n,-1);
+//    return solve(0,st,et,pf,dp);  
+//     }
+int find(int start, int endTime, vector<int>& st) {
+    int l = start, r = st.size() - 1, ans = st.size();
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (st[mid] < endTime) l = mid + 1;
+        else {
+            ans = mid;
+            r = mid - 1;
         }
     }
     return ans;
 }
-int solve(int i,vector<int>& startTime, vector<int>& endTime, vector<int>& profit,vector<int>&dp){
-    // basecase
-    if(i==n)return 0;
-
-     if( dp[i]!=-1)return dp[i];
-
-    int skip=solve(i+1,startTime,endTime,profit,dp);
-    int next_id = find(i,endTime[i],startTime);
-    int take=profit[i] + solve(next_id,startTime,endTime,profit,dp);
-    return dp[i]=max(take,skip);
-}
+int n;
 int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
     n=startTime.size();
     vector<int>idx(n);
@@ -40,7 +70,16 @@ int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& pro
         et[i]=endTime[idx[i]];
         pf[i]=profit[idx[i]];
     }
-    vector<int>dp(n,-1);
-   return solve(0,st,et,pf,dp);  
+    vector<int>dp(n+1,0);
+
+    for (int i = n-1; i>=0; i--)
+    {
+        int skip=dp[i+1];
+        int next_id=find(i+1,et[i],st);
+        int take=take=profit[i]+dp[next_id];
+
+        dp[i]=max(take,skip);
+    }
+    return dp[0];
     }
 };
